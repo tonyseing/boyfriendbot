bb0a62a267a1019b6a40ed47e51b2477f64f012b
Tony Seing <tony.seing@gmail.com>
2014-09-24 06:41:37 -0400

    message works with twilio

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.0.0"]
-                 [twilio-api "1.0.0"]
+                 [com.cemerick/url "0.1.1"]
                  ])
--- src/boyfriendbot/message.clj
+++ src/boyfriendbot/message.clj
@@ -1,10 +1,14 @@
 (ns boyfriendbot.message
-  (:require [twilio.core :as twilio]))
+  (:require [clj-http.client :as client]
+            [cemerick.url :refer (url url-encode)]))
 
-(defn send-txt-msg [sid auth-token sender recipient message]
-  "Function takes a recipient phone number and a message and then sends a text message to that number"
-  (let [response (twilio/with-auth sid auth-token
-            (twilio/send-sms {:from sender
-                              :to recipient
-                              :body message}))]
-    (println response)) )
+
+(def posturl (str "https://api.twilio.com/2010-04-01/Accounts/" accountSid  "/Messages.json"))
+(def msg  { :Body "clojuretest"  :To "+18608306959" :From "+18603560847"})
+
+(client/post posturl
+             { :basic-auth [accountSid auth-token]
+               :accept :json
+               :form-params msg})
+
+
