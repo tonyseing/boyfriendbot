cd8564c6ed1a051fc93794ddaa00d6a02a7b741a
Tony Seing <tony.seing@gmail.com>
2014-09-25 03:30:57 -0400

    takes in schedule from config file

--- src/boyfriendbot/core.clj
+++ src/boyfriendbot/core.clj
@@ -15,11 +15,12 @@
     (println (str "Message sent: " msg))))
 
 
+(let [schedule (:schedule config)]
+  (def send-gf-msg-task  {:id "send-gf-msg-task"
+                          :handler send-gf-msg-handler
+                          :schedule schedule  ;; sends out message at 6 everyday,  second minute hour day-of-week day-of-month month year
+                          :opts {:output "Hello There"}}))
 
-(def send-gf-msg-task  {:id "send-gf-msg-task"
-                        :handler send-gf-msg-handler
-                        :schedule "/2 * * * * * *"
-                        :opts {:output "Hello There"}})
 
 
 (def cj (cron/cronj :entries [send-gf-msg-task]))
