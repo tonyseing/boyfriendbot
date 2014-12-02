8a4518ec2726ef4a09baf2a97487abd605249c6b
Tony Seing <tony.seing@gmail.com>
2014-12-02 01:00:01 -0500

    refactors main function to remove def statements and replace with let bindings

--- src/boyfriendbot/core.clj
+++ src/boyfriendbot/core.clj
@@ -8,46 +8,35 @@
 (def config (json/parse-string (slurp "config.json") true))
 
 
-
-
 (defn send-gf-msg-handler
   ""
   [t opts]
   (let [msgs (:messages config)
         auth (:auth config)
         chosen-msg-indx (rand-int (count (:messages config)))
-        msg  (nth msgs chosen-msg-indx) ]
-
+        msg  (nth msgs chosen-msg-indx)]
     (message/send-text-message msg auth)
     (println (str "Message sent: " msg))
-    (println "Press q and *ENTER* to quit")
-    ))
+    (println "Press q and *ENTER* to quit")))
 
 (defn -main
   []
-
-  (let [schedule (:schedule config)] 
-    (def send-gf-msg-task  {:id "send-gf-msg-task"
-                            :handler send-gf-msg-handler
-                          :schedule schedule  ;; sends out message, second minute hour day-of-week day-of-month month year
-                          :opts {:output "Sent."}}))
-
-  
-  (def cj (cron/cronj :entries [send-gf-msg-task]))
-  
-  (def keep-running? (atom true))
-
-  
-  (cron/start! cj)
-  (println "Boyfriend-Bot has begun. Press q and *ENTER* to stop Boyfriend-Bot.")
-  
-  (while @keep-running?
-    (if (= (read-line) "q")
-      (do
-        (cron/stop! cj)
-        (reset! keep-running? false)
-        (println  "Boyfriend-Bot has terminated.")
-        (System/exit 0)))))
+  (let [schedule (:schedule config)
+        send-gf-msg-task  {:id "send-gf-msg-task"
+                           :handler send-gf-msg-handler
+                           :schedule schedule  ;; sends out message, second minute hour day-of-week day-of-month month year
+                           :opts {:output "Sent."}}
+        cj (cron/cronj :entries [send-gf-msg-task])
+        keep-running? (atom true)]
+    (cron/start! cj)
+    (println "Boyfriend-Bot has begun. Press q and *ENTER* to stop Boyfriend-Bot.")
+    (while @keep-running?
+      (if (= (read-line) "q")
+        (do
+          (cron/stop! cj)
+          (reset! keep-running? false)
+          (println  "Boyfriend-Bot has terminated.")
+          (System/exit 0))))))
 
  
 
